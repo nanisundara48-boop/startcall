fc27c6d4d1d0df6b978de30d6f6564d004482851
nanisundara48-boop <nanisundara48@gmail.com>
2026-06-01 14:24:00 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Background Service Worker Engine
+self.addEventListener('push', function(event) {
+    // Firebase push handles trigger down directly inside structural layout
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+            for (var i = 0; i < clientList.length; i++) {
+                var client = clientList[i];
+                if ('focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+    );
+});
